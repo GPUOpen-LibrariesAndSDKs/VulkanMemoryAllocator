188a365ed48e03e0a09b636464d4c16a00e14899
Adam Sawicki <Adam.Sawicki@amd.com>
2019-07-26 15:22:51 +0200

    Fix ignore warning C4324 also in VmaReplay project
    See #71

--- src/VmaReplay/VmaUsage.h
+++ src/VmaReplay/VmaUsage.h
@@ -43,6 +43,7 @@
 #pragma warning(disable: 4127) // conditional expression is constant
 #pragma warning(disable: 4100) // unreferenced formal parameter
 #pragma warning(disable: 4189) // local variable is initialized but not referenced
+#pragma warning(disable: 4324) // structure was padded due to alignment specifier
 
 #include "../vk_mem_alloc.h"
 
