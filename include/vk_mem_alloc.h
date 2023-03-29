383f06b9fd9d36fb54cb3d4ac3363a670104bc62
Stephan Hartmann <stha09@googlemail.com>
2023-03-29 17:30:34 +0000

    IWYU: add cstdio for snprintf in vk_mem_alloc.h
    Discoverd with upcoming GCC 13.

--- include/vk_mem_alloc.h
+++ include/vk_mem_alloc.h
@@ -2602,6 +2602,7 @@ VMA_CALL_PRE void VMA_CALL_POST vmaFreeStatsString(
 #undef VMA_IMPLEMENTATION
 
 #include <cstdint>
+#include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <utility>
