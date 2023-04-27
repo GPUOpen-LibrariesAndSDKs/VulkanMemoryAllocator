f95eea7aa488c1b5a44a1edcda58fc293bd6aa35
Marcin Zając <marcin@pingpongcat.com>
2023-04-27 18:13:15 +0200

    Missing cstdio
    Under Fedora 38 g++ (GCC) 13.0.1 I was getting:
    
    vk_mem_alloc.h:2839:5: error: ‘snprintf’ was not declared in this scope
     2839 |     snprintf(outStr, strLen, "%u", static_cast<unsigned int>(num));

--- include/vk_mem_alloc.h
+++ include/vk_mem_alloc.h
@@ -2687,6 +2687,7 @@ remove them if not needed.
     #include <cassert> // for assert
     #include <algorithm> // for min, max
     #include <mutex>
+    #include <cstdio>
 #else
     #include VMA_CONFIGURATION_USER_INCLUDES_H
 #endif
