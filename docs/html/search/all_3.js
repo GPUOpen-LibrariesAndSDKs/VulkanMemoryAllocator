d802b362c6c4e0494a05facabad7368df1359272
Adam Sawicki <adam.sawicki@amd.com>
2024-02-13 11:56:59 +0100

    Rebuilt the documentation

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -10,13 +10,14 @@ var searchData=
   ['defragmentation_7',['Defragmentation',['../defragmentation.html',1,'index']]],
   ['deprecated_20list_8',['Deprecated List',['../deprecated.html',1,'']]],
   ['detection_9',['Corruption detection',['../debugging_memory_usage.html#debugging_memory_usage_corruption_detection',1,'']]],
-  ['device_10',['device',['../struct_vma_allocator_create_info.html#ad924ddd77b04039c88d0c09b0ffcd500',1,'VmaAllocatorCreateInfo::device'],['../struct_vma_allocator_info.html#a012b4c485bf3b0ea8921352c5ee0c357',1,'VmaAllocatorInfo::device']]],
-  ['device_20address_11',['Enabling buffer device address',['../enabling_buffer_device_address.html',1,'index']]],
-  ['device_20heap_20memory_20limit_12',['Device heap memory limit',['../configuration.html#heap_memory_limit',1,'']]],
-  ['device_20memory_20allocation_20callbacks_13',['Device memory allocation callbacks',['../configuration.html#allocation_callbacks',1,'']]],
-  ['devicememory_14',['deviceMemory',['../struct_vma_allocation_info.html#ae0bfb7dfdf79a76ffefc9a94677a2f67',1,'VmaAllocationInfo']]],
-  ['devicememoryblocksfreed_15',['deviceMemoryBlocksFreed',['../struct_vma_defragmentation_stats.html#a0113f1877904a5d1ee8f409216ff276b',1,'VmaDefragmentationStats']]],
-  ['double_20stack_16',['Double stack',['../custom_memory_pools.html#linear_algorithm_double_stack',1,'']]],
-  ['dsttmpallocation_17',['dstTmpAllocation',['../struct_vma_defragmentation_move.html#ab65b106adf209acd7313296d1075300e',1,'VmaDefragmentationMove']]],
-  ['dump_18',['JSON dump',['../statistics.html#statistics_json_dump',1,'']]]
+  ['detection_20features_10',['Leak detection features',['../debugging_memory_usage.html#debugging_memory_usage_leak_detection',1,'']]],
+  ['device_11',['device',['../struct_vma_allocator_create_info.html#ad924ddd77b04039c88d0c09b0ffcd500',1,'VmaAllocatorCreateInfo::device'],['../struct_vma_allocator_info.html#a012b4c485bf3b0ea8921352c5ee0c357',1,'VmaAllocatorInfo::device']]],
+  ['device_20address_12',['Enabling buffer device address',['../enabling_buffer_device_address.html',1,'index']]],
+  ['device_20heap_20memory_20limit_13',['Device heap memory limit',['../configuration.html#heap_memory_limit',1,'']]],
+  ['device_20memory_20allocation_20callbacks_14',['Device memory allocation callbacks',['../configuration.html#allocation_callbacks',1,'']]],
+  ['devicememory_15',['deviceMemory',['../struct_vma_allocation_info.html#ae0bfb7dfdf79a76ffefc9a94677a2f67',1,'VmaAllocationInfo']]],
+  ['devicememoryblocksfreed_16',['deviceMemoryBlocksFreed',['../struct_vma_defragmentation_stats.html#a0113f1877904a5d1ee8f409216ff276b',1,'VmaDefragmentationStats']]],
+  ['double_20stack_17',['Double stack',['../custom_memory_pools.html#linear_algorithm_double_stack',1,'']]],
+  ['dsttmpallocation_18',['dstTmpAllocation',['../struct_vma_defragmentation_move.html#ab65b106adf209acd7313296d1075300e',1,'VmaDefragmentationMove']]],
+  ['dump_19',['JSON dump',['../statistics.html#statistics_json_dump',1,'']]]
 ];
